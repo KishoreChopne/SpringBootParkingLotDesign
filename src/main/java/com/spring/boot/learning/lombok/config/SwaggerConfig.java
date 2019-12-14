656dcfb854c712f5a0257a97a0d7ee6cc887862b
Kishore Chopne <kishor@Kishores-MacBook-Pro.local>
2019-12-14 09:22:13 +0530

    Swagger documentation

--- src/main/java/com/spring/boot/learning/lombok/config/SwaggerConfig.java
+++ src/main/java/com/spring/boot/learning/lombok/config/SwaggerConfig.java
@@ -3,7 +3,10 @@ package com.spring.boot.learning.lombok.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,11 +17,15 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage(
-						"com.spring.boot.learning.lombok.controller"))
-				.build();
+		return new Docket(DocumentationType.SWAGGER_2).groupName("Parking-api").apiInfo(apiInfo()).select()
+				.apis(RequestHandlerSelectors.basePackage("com.spring.boot.learning.lombok.controller")).build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Parking Lot API").description("Parking Lot API reference for developers")
+				.termsOfServiceUrl("http://JustDummywebsite.com")
+				.contact(new Contact("Parking Lot", "http://JustDummywebsite.com", "parkinglot@gmail.com"))
+				.license("Parking Lot License").licenseUrl("parkinglot@gmail.com").version("1.0").build();
 	}
 
 }
-
