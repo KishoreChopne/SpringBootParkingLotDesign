2c964af9b2302b04398e867ed79d0ef7b8824b1e
kishor <kishor@192.168.0.105>
2020-01-12 09:04:42 +0530

    "Testing CICD Deployment

--- src/main/java/com/spring/boot/learning/lombok/controller/ExampleController.java
+++ src/main/java/com/spring/boot/learning/lombok/controller/ExampleController.java
@@ -43,7 +43,7 @@ public class ExampleController {
 	
 	@GetMapping("/hellojenkins")
 	public String helloJenkins() {
-		return "Hello Jenkins";
+		return "Hello Jenkin";
 	}
 //
 //	@PostMapping("/myInfo/{name}/{experiance}/{skills}")
